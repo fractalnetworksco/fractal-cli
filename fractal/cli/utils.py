3f9a3711a6986edbc426dec7fee83ee2474b32fa
Justin <jrussel1055@gmail.com>
2023-12-05 17:13:30 -0600

    Move to fractal-matrix

--- fractal/cli/utils.py
+++ fractal/cli/utils.py
@@ -53,33 +53,3 @@ def read_user_data(filename: str) -> Tuple[Dict[str, Any], str]:
         raise error
 
     return user_data, data_file_path
-
-
-def prompt_matrix_password(matrix_id: str) -> str:
-    """
-    Prompts for Matrix password.
-
-    Args:
-        matrix_id: Matrix ID to prompt for
-
-    TODO: This should instead direct to a homeserver login page.
-    """
-    print(f"Login with Matrix ID ({matrix_id}) to continue")
-    try:
-        password = getpass(f"{matrix_id}'s password: ")
-        return password
-    except (KeyboardInterrupt, EOFError):
-        # newline after ^C
-        print()
-        exit(1)
-
-
-def parse_matrix_id(matrix_id: str) -> Tuple[str, str]:
-    """Parse local part and homeserver from MatrixID"""
-    pattern = re.compile(r"^@([^:]+):([^:]+)$")
-    match = pattern.match(matrix_id)
-    if not match:
-        raise InvalidMatrixIdException(f"{matrix_id} is not a valid Matrix ID.")
-    user_localpart = match.group(1)
-    homeserver = match.group(2)
-    return (user_localpart, homeserver)
