4aeaa7bfdc27e43ff3524262ef759c8d3d11ef47
Justin <jrussel1055@gmail.com>
2024-01-17 15:30:38 -0600

    adds fixture that removes userdatadir at end of test

--- conftest.py
+++ conftest.py
@@ -1,9 +1,21 @@
+import os
 from unittest.mock import patch
 
 import pytest
+from fractal.cli import FRACTAL_DATA_DIR
 
 
 @pytest.fixture
 def mock_getpass():
     with patch("fractal.matrix.utils.getpass", return_value="admin"):
         yield
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    yield
+
+    try:
+        os.removedirs(FRACTAL_DATA_DIR)
+    except FileNotFoundError:
+        pass
