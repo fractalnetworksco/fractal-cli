ca1dcc4bd6aaa092e5ae27fc58db1e77dbaf27f8
Jonathan <jonathan.leal1231@gmail.com>
2024-02-01 18:00:14 -0500

    full coverage for registration.py. just need docstrings

--- tests/test_registration_controller.py
+++ tests/test_registration_controller.py
@@ -79,6 +79,7 @@ async def test_registration_controller_register_local_user_id_already_taken():
 
 async def test_registration_controller_register_True_local(test_registration_token):
     """
+    Tests that _register_local is called when True is passed as the local parameter.
     """
 
     matrix_id = f"@test-user-{secrets.token_hex(8)}:localhost"
@@ -101,6 +102,8 @@ async def test_registration_controller_register_True_local(test_registration_tok
 
 async def test_registration_controller_register_existing_homeserver(test_registration_token, test_homeserver_url):
     """
+    Tests that get_homeserver_for_matrix_id is not called if there is already an existing
+    homeserver_url passed to the function.
     """
 
     matrix_id = f"@test-user-{secrets.token_hex(8)}:localhost"
@@ -122,6 +125,8 @@ async def test_registration_controller_register_existing_homeserver(test_registr
 
 async def test_registration_controller_register_no_homeserver(test_registration_token):
     """
+    Tests that get_homeserver_for_matrix_id is called if there is no existing homeserver_url
+    passed to the function and local is set to False.
     """
 
     matrix_id = f"@test-user-{secrets.token_hex(8)}:localhost"
@@ -139,6 +144,62 @@ async def test_registration_controller_register_no_homeserver(test_registration_
 
     assert homeserver_url is not None 
 
-# async def test_registration_controller_register
+def test_registration_controller_register_cli_method(test_registration_token):
+    """
+    Tests that _register is called when the register cli method is called.
+    """
+
+    matrix_id = f"@test-user-{secrets.token_hex(8)}:localhost"
+    password = "test_password"
+
+    test_registration_controller = RegistrationController()
+
+    with patch('fractal.cli.controllers.registration.RegistrationController._register') as mock_register:
+        mock_register.return_value = ["test", "value"]
+        test_registration_controller.register(
+            matrix_id=matrix_id,
+            password=password,
+            registration_token=test_registration_token,
+        )
+
+    mock_register.assert_called()
+
+async def test_registration_controller_create_token():
+    """
+    Tests that generate_registration_token is called when _create_token is called.
+    """
+
+    test_registration_controller = RegistrationController()
+    
+    token = None
+
+    token = await test_registration_controller._create_token()
+
+    assert token is not None
+
+def test_registration_controller_token_cases():
+    """
+    Tests the cases for the token method.
+
+    NOTE: not all cases have been implemented yet
+    """
+
+    # creating a token
+    test_registration_controller = RegistrationController()
+    token = None
+    token = test_registration_controller.token('create')
+    assert token is not None
+
+    # list case
+    with patch('fractal.cli.controllers.registration.print') as mock_print:
+        # TODO: update this test when the list case gets implemented
+        test_registration_controller.token('list')
+    mock_print.assert_called_once_with('FIXME: List not implemented yet.')
+
+    # invalid action case
+    with patch('fractal.cli.controllers.registration.print') as mock_print:
+        test_registration_controller.token('invalid_action')
+    # TODO: might need to update the string if it gets changed in the token function
+    mock_print.assert_called_once_with("Invalid action. Must be either 'create'")
 
 
